5195c4b1ec90c4d403380a6d939f49b67626e744
bo-peng123 <2967479124@qq.com>
2026-03-25 12:00:11 +0000

    Deploying to gh-pages from @ bo-peng123/bo-peng123.github.io@1559fa640f7d9c3d3cb6752a579e3fdabb2d417f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
+  },{id: "nav-papers",
+          title: "papers",
+          description: "publications by categories in reversed chronological order. Authors are listed in alphabetical order.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-cv",
           title: "CV",
           description: "",
           section: "Navigation",
